eaff14f53c192aa3af764165879afba9d30652bb
wylenjoan <57890826+wylenjoan@users.noreply.github.com>
2021-10-07 21:21:45 +0800

    Add robofriends

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooooops.</h1>;
+    }
+  }
+}
+
+export default ErrorBoundary;
